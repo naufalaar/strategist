2b9f365edcd26b9617f4fde4a969a0ca134cb5f8
naufalaar <naufal.aar@gmail.com>
2023-03-19 09:31:46 +0000

    Added U1000

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,39 +1,38 @@
 <template>
   <b-container>
     <h2 class="mt-4">Average Wages</h2>
-    <b-form class="mt-4">
-      <b-row>
-        <b-col cols="4">
-          <b-form-input
-            id="inline-form-input-name"
-            class="mb-2 mr-sm-2 mb-sm-0"
-            placeholder="Team IDs"
-            v-model="teamIds"
-          ></b-form-input>
-        </b-col>
-        <b-col cols="4">
-          <b-button variant="primary" @click="getTeams">Get Details</b-button>
-        </b-col>
-      </b-row>
-    </b-form>
+    <b-row>
+      <b-col cols="4">
+        <b-form-input
+          id="inline-form-input-name"
+          class="mb-2 mr-sm-2 mb-sm-0"
+          placeholder="Team IDs"
+          v-model="teamIds"
+        ></b-form-input>
+      </b-col>
+      <b-col cols="4">
+        <b-form-checkbox v-model="under" class="mb-2 mr-sm-2 mb-sm-0">U1000</b-form-checkbox>
+      </b-col>
+      <b-col cols="4">
+        <b-button variant="primary" @click="getTeams">Get Details</b-button>
+      </b-col>
+    </b-row>
 
     <b-table striped hover :items="wageTable"></b-table>
     <h2 class="mt-4">Match Stats</h2>
-    <b-form class="mt-4">
-      <b-row>
-        <b-col cols="4">
-          <b-form-input
-            id="inline-form-input-name"
-            class="mb-2 mr-sm-2 mb-sm-0"
-            placeholder="Match IDs"
-            v-model="matchIds"
-          ></b-form-input>
-        </b-col>
-        <b-col cols="4">
-          <b-button variant="primary" @click="getMatch">Get Details</b-button>
-        </b-col>
-      </b-row>
-    </b-form>
+    <b-row>
+      <b-col cols="4">
+        <b-form-input
+          id="inline-form-input-name"
+          class="mb-2 mr-sm-2 mb-sm-0"
+          placeholder="Match IDs"
+          v-model="matchIds"
+        ></b-form-input>
+      </b-col>
+      <b-col cols="4">
+        <b-button variant="primary" @click="getMatch">Get Details</b-button>
+      </b-col>
+    </b-row>
     <h3 class="mt-4">Batting</h3>
     <b-table striped hover :items="matchTable.batting"></b-table>
     <h3 class="mt-4">Bowling</h3>
@@ -68,7 +67,7 @@ export default {
         bowing: [],
         team: [],
       };
-      
+
       var headers = this.stats["batting"][0].split(",");
       for (var i = 1; i < this.stats["batting"].length; i++) {
         var data = this.stats["batting"][i].split(",");
@@ -105,18 +104,25 @@ export default {
   data() {
     return {
       teamIds: "0",
+      under: false,
       matchIds: "0",
       wages: [
         "TeamID, TeamName, Average, Median, NumberOfPlayers",
         "Dummy,Dummy,Dummy,Dummy,Dummy",
       ],
       stats: {
-        batting: ["PlayerID, Name, Team(s), Matches, Innings, Runs, Balls, NO, Average, StrikeRate, 4s, 6s, 50s, 100s, HS",
-        "Dummy,Dummy,Dummy,Dummy,Dummy,Dummy,Dummy,Dummy,Dummy,Dummy,Dummy,Dummy,Dummy,Dummy,Dummy"],
-        bowing: ["PlayerID, Name, Team(s), Matches, Overs, Balls, Maidens, Runs, Wickets, Average, StrikeRate, Economy, NoBalls, Wides, 5wi, Best",
-        "Dummy,Dummy,Dummy,Dummy,Dummy,Dummy,Dummy,Dummy,Dummy,Dummy,Dummy,Dummy,Dummy,Dummy,Dummy,Dummy"],
-        team: ["TeamID, Team, Total, Wins, Losses, Ties, WinPercentage",
-        "Dummy,Dummy,Dummy,Dummy,Dummy,Dummy,Dummy"],
+        batting: [
+          "PlayerID, Name, Team(s), Matches, Innings, Runs, Balls, NO, Average, StrikeRate, 4s, 6s, 50s, 100s, HS",
+          "Dummy,Dummy,Dummy,Dummy,Dummy,Dummy,Dummy,Dummy,Dummy,Dummy,Dummy,Dummy,Dummy,Dummy,Dummy",
+        ],
+        bowing: [
+          "PlayerID, Name, Team(s), Matches, Overs, Balls, Maidens, Runs, Wickets, Average, StrikeRate, Economy, NoBalls, Wides, 5wi, Best",
+          "Dummy,Dummy,Dummy,Dummy,Dummy,Dummy,Dummy,Dummy,Dummy,Dummy,Dummy,Dummy,Dummy,Dummy,Dummy,Dummy",
+        ],
+        team: [
+          "TeamID, Team, Total, Wins, Losses, Ties, WinPercentage",
+          "Dummy,Dummy,Dummy,Dummy,Dummy,Dummy,Dummy",
+        ],
       },
       host: "https://battrickstats-d4otn6fbfa-uc.a.run.app/",
     };
@@ -130,12 +136,12 @@ export default {
         },
       };
       const requestBody = {
-        U1000: true,
+        U1000: this.under,
         teamIDs: this.teamIds.split(",").map(Number),
       };
       console.log(requestBody);
       await axios
-        .post(this.host+"averageWages", requestBody, headers)
+        .post(this.host + "averageWages", requestBody, headers)
         .then((response) => {
           console.log(response.data.wages);
           this.wages = response.data.wages;
@@ -152,12 +158,20 @@ export default {
         matchIDs: this.matchIds.split(",").map(Number),
       };
       await axios
-        .post(this.host+"stats", requestBody, headers)
+        .post(this.host + "stats", requestBody, headers)
         .then((response) => {
           console.log(response.data);
           this.stats = response.data;
         })
-        .catch(console.log("Error"));
+        .catch(() => {
+          this.$bvToast.toast(`Error while retrieving stats`, {
+            title: "Retrieve Failed",
+            autoHideDelay: 500000,
+            appendToast: true,
+            noCloseButton: true,
+            variant: "danger",
+          });
+        });
     },
   },
 };
